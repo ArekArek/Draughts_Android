941489551f30dc0fd4371e30d2800fc588f54d09
Arek <arek314arek@gmail.com>
2018-10-13 16:02:26 +0200

    Created first version of game activity

---
+++ app/src/main/java/com/tuco/draughtsui/game/GameActivity.java
@@ -0,0 +1,87 @@
+package com.tuco.draughtsui.game;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.tuco.draughts.board.util.StandardBoardCreator;
+import com.tuco.draughts.game.DraughtGameManager;
+import com.tuco.draughts.game.DraughtsState;
+import com.tuco.draughts.game.util.ChangeTurnListener;
+import com.tuco.draughts.movement.maker.AIMovementMaker;
+import com.tuco.draughts.movement.maker.AlgorithmType;
+import com.tuco.draughts.movement.maker.Heuristic;
+import com.tuco.draughts.movement.maker.MovementMaker;
+import com.tuco.draughtsui.R;
+import com.tuco.draughtsui.game.board.BoardView;
+
+
+public class GameActivity extends AppCompatActivity {
+
+    private DraughtGameManager gameManager;
+    private BoardView boardView;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        initializeGame();
+
+        initializeViews();
+    }
+
+    private void initializeGame() {
+        ChangeTurnListener generalChangeTurnListener = new ChangeTurnListener() {
+            @Override
+            public void afterTurn() {
+                Runnable runnable = () -> updateScreen();
+                runOnUiThread(runnable);
+            }
+        };
+
+        DraughtsState state = new DraughtsState(new StandardBoardCreator());
+
+        MovementMaker aiMove1 = new AIMovementMaker(state, AlgorithmType.ALPHABETA, Heuristic.SIMPLE);
+        MovementMaker aiMove2 = new AIMovementMaker(state, AlgorithmType.SCOUT, Heuristic.SIMPLE);
+
+        gameManager = DraughtGameManager.builder()
+                .state(state)
+                .playerWhite(aiMove2)
+                .playerBlack(aiMove1)
+                .generalChangeTurnListener(generalChangeTurnListener)
+                .build();
+    }
+
+    private void initializeViews() {
+        boardView = findViewById(R.id.boardView);
+        boardView.init(gameManager.getState().getBoard());
+
+        startButton = findViewById(R.id.startGameButton);
+        startButton.setOnClickListener(view -> {
+            startButton.setClickable(false);
+
+            Runnable runnable = () -> gameManager.play();
+            Thread thread = new Thread(runnable);
+            thread.start();
+        });
+    }
+
+    private void updateScreen() {
+        boardView.update(gameManager.getState().getBoard());
+        if (gameManager.isPlaying()) {
+            switch (gameManager.getState().getPlayer()) {
+                case WHITE:
+                    startButton.setText("Player WHITE");
+                    break;
+                case BLACK:
+                    startButton.setText("Player BLACK");
+                    break;
+            }
+        }
+    }
+
+
+}
