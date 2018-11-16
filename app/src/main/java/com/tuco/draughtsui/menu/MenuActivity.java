fc4986e804f9085aee2a8acd485a8ec818f3ef5f
Arek <arek314arek@gmail.com>
2018-11-16 08:07:47 +0100

    Create menu activity

---
+++ app/src/main/java/com/tuco/draughtsui/menu/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.tuco.draughtsui.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tuco.draughtsui.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
