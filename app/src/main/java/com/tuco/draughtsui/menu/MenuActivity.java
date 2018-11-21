d30fb32c4e746672ecaad23c25bd767dbfcff606
Arek <arek314arek@gmail.com>
2018-11-21 20:13:42 +0100

    Passing player configurations to game activity

--- app/src/main/java/com/tuco/draughtsui/menu/MenuActivity.java
+++ app/src/main/java/com/tuco/draughtsui/menu/MenuActivity.java
@@ -1,15 +1,40 @@
 package com.tuco.draughtsui.menu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.tuco.draughtsui.R;
+import com.tuco.draughtsui.game.GameActivity;
+import com.tuco.draughtsui.menu.configuration.PlayerConfigurationView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private PlayerConfigurationView playerWhiteConfiguration;
+    private PlayerConfigurationView playerBlackConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        initViews();
+    }
+
+    private void initViews() {
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener((l) -> startGameActivity());
+
+        playerWhiteConfiguration = findViewById(R.id.playerWhiteConfiguration);
+        playerBlackConfiguration = findViewById(R.id.playerBlackConfiguration);
+    }
+
+    private void startGameActivity() {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("whiteConfiguration", playerWhiteConfiguration.getData());
+        intent.putExtra("blackConfiguration", playerBlackConfiguration.getData());
+        startActivity(intent);
     }
 }
