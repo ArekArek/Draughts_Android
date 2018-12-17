a9778a0aa7c6f38982d4bc3137b140f8a95c9d29
Arek <arek314arek@gmail.com>
2018-12-17 10:04:30 +0100

    Separate game view and logic

--- app/src/main/java/com/tuco/draughtsui/game/GameActivity.java
+++ app/src/main/java/com/tuco/draughtsui/game/GameActivity.java
@@ -4,28 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
-import com.tuco.draughts.board.util.StandardBoardCreator;
-import com.tuco.draughts.game.DraughtGameManager;
-import com.tuco.draughts.game.DraughtsState;
-import com.tuco.draughts.game.util.ChangeTurnListener;
-import com.tuco.draughts.movement.maker.MovementMaker;
 import com.tuco.draughtsui.R;
 import com.tuco.draughtsui.game.board.BoardView;
-import com.tuco.draughtsui.game.movement.GameTurnChanger;
-import com.tuco.draughtsui.game.movement.MovementMakerCreator;
 import com.tuco.draughtsui.menu.MenuActivity;
-import com.tuco.draughtsui.menu.configuration.PlayerConfigurationDTO;
 
 
 public class GameActivity extends AppCompatActivity {
 
-    private static DraughtGameManager gameManager;
     private BoardView boardView;
     private Button startButton;
     private Button backToMenuButton;
-    private static Thread gameManagerThread;
+    private GameActivityLogic gameActivityLogic;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,70 +26,27 @@ public class GameActivity extends AppCompatActivity {
 
         boardView = findViewById(R.id.boardView);
 
-        initializeGame();
+        gameActivityLogic = new GameActivityLogic(this, boardView);
+        gameActivityLogic.initializeGame();
 
         initializeViews();
     }
 
-    private void initializeGame() {
-        DraughtsState state = new DraughtsState(new StandardBoardCreator());
-        MovementMaker playerWhite = createPlayer(state, "whiteConfiguration");
-        MovementMaker playerBlack = createPlayer(state, "blackConfiguration");
-        ChangeTurnListener generalChangeTurnListener = new GameTurnChanger(boardView, this);
-
-        gameManager = DraughtGameManager.builder()
-                .state(state)
-                .playerWhite(playerWhite)
-                .playerBlack(playerBlack)
-                .generalChangeTurnListener(generalChangeTurnListener)
-                .build();
-    }
-
-    private MovementMaker createPlayer(DraughtsState state, String extraName) {
-        PlayerConfigurationDTO whitePlayerConfiguration = getIntent().getExtras().getParcelable(extraName);
-        return MovementMakerCreator.create(whitePlayerConfiguration, boardView, state);
-    }
 
     private void initializeViews() {
-        boardView.init(gameManager.getState().getBoard());
+        boardView.init(gameActivityLogic.getBoard());
 
         startButton = findViewById(R.id.startGameButton);
         startButton.setOnClickListener(view -> startGame());
 
         backToMenuButton = findViewById(R.id.backToMenuButton);
         backToMenuButton.setOnClickListener(view -> backToMenu());
-
     }
 
     private void startGame() {
         startButton.setClickable(false);
-        refreshButtonText();
-        startGameThread();
-    }
-
-    private void startGameThread() {
-        if (gameManagerThread != null) {
-            try {
-                gameManagerThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        gameManagerThread = new Thread(() -> gameManager.play());
-        gameManagerThread.start();
-    }
-
-    private void refreshButtonText() {
-        if (gameManager.isPlaying()) {
-            switch (gameManager.getState().getPlayer()) {
-                case WHITE:
-                    startButton.setText("Player WHITE");
-                    break;
-                case BLACK:
-                    startButton.setText("Player BLACK");
-                    break;
-            }
-        }
+        startButton.setVisibility(View.GONE);
+        gameActivityLogic.startGameThread();
     }
 
     private void backToMenu() {
@@ -106,14 +55,12 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void update() {
-        boardView.update(gameManager.getState().getBoard());
-
-        refreshButtonText();
+        boardView.update(gameActivityLogic.getBoard());
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        gameManager.stopGame();
+        gameActivityLogic.stopGameThread();
     }
 }
---
+++ app/src/main/java/com/tuco/draughtsui/game/GameActivityLogic.java
@@ -0,0 +1,66 @@
+package com.tuco.draughtsui.game;
+
+import com.tuco.draughts.board.Board;
+import com.tuco.draughts.board.util.StandardBoardCreator;
+import com.tuco.draughts.game.DraughtGameManager;
+import com.tuco.draughts.game.DraughtsState;
+import com.tuco.draughts.game.util.ChangeTurnListener;
+import com.tuco.draughts.movement.maker.MovementMaker;
+import com.tuco.draughtsui.game.board.BoardView;
+import com.tuco.draughtsui.game.movement.GameTurnChanger;
+import com.tuco.draughtsui.game.movement.MovementMakerCreator;
+import com.tuco.draughtsui.menu.configuration.PlayerConfigurationDTO;
+
+public class GameActivityLogic {
+
+    private GameActivity gameActivity;
+    private BoardView boardView;
+    private DraughtGameManager gameManager;
+    private Thread gameManagerThread;
+
+    public GameActivityLogic(GameActivity gameActivity, BoardView boardView) {
+        this.gameActivity = gameActivity;
+        this.boardView = boardView;
+    }
+
+    public void initializeGame() {
+        DraughtsState state = new DraughtsState(new StandardBoardCreator());
+
+        MovementMaker playerWhite = createPlayer(state, "whiteConfiguration");
+        MovementMaker playerBlack = createPlayer(state, "blackConfiguration");
+
+        ChangeTurnListener generalChangeTurnListener = new GameTurnChanger(boardView, gameActivity);
+
+        gameManager = DraughtGameManager.builder()
+                .state(state)
+                .playerWhite(playerWhite)
+                .playerBlack(playerBlack)
+                .generalChangeTurnListener(generalChangeTurnListener)
+                .build();
+    }
+
+    private MovementMaker createPlayer(DraughtsState state, String extraName) {
+        PlayerConfigurationDTO whitePlayerConfiguration = gameActivity.getIntent().getExtras().getParcelable(extraName);
+        return MovementMakerCreator.create(whitePlayerConfiguration, boardView, state);
+    }
+
+    public void startGameThread() {
+        if (gameManagerThread != null) {
+            try {
+                gameManagerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        gameManagerThread = new Thread(() -> gameManager.play());
+        gameManagerThread.start();
+    }
+
+    public void stopGameThread() {
+        gameManager.stopGame();
+    }
+
+    public Board getBoard() {
+        return gameManager.getState().getBoard();
+    }
+}
